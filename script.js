01f68b99a7b16307b5ccf4dcc8a2cee3e1a92255
Israel Vieira <israviei.dev@gmail.com>
2026-09-10 18:56:33 -0300

    implementação do esqueci senha, adicionando icone de olho para ocultar e mostrar senha adicionando a biblioteca lucide e script, posicionando o olho com css dentro do input

---
+++ script.js
@@ -0,0 +1,18 @@
+const senha = document.querySelector("#senha");
+const botaoOlho = document.querySelector("#id-icon-eye");
+
+botaoOlho.addEventListener("click", function () {
+
+    if (senha.type === "password") {
+        senha.type = "text";
+
+        botaoOlho.innerHTML = '<i data-lucide="eye"></i>';
+        lucide.createIcons();
+    } else {
+        senha.type = "password";
+
+        botaoOlho.innerHTML = '<i data-lucide="eye-closed"></i>';
+        lucide.createIcons();
+    }
+
+});
