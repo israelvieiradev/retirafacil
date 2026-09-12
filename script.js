9b7d81edda091b549c08e58c46268bb4187a9bcf
Israel Vieira <israviei.dev@gmail.com>
2026-09-11 21:26:37 -0300

    implementação de redes sociais, rede sociais responsivas onde no celular pode acessar pelo menu hamburger, implementação de rodapé

--- script.js
+++ script.js
@@ -1,6 +1,30 @@
 const senha = document.querySelector("#senha");
 const botaoOlho = document.querySelector("#id-icon-eye");
 
+const hamburger = document.querySelector("#btn-hamburger");
+const menu = document.querySelector(".nav-links");
+
+//ao clicar no hamburger muda o display do menu de flex para none ou vice-versa
+hamburger.addEventListener("click", () => {
+    if (menu.style.display === "flex") {
+        menu.style.display = "none";
+    } else {
+        menu.style.display = "flex";
+    }
+});
+
+//muda o display do menu para flex quando maior que 768px
+window.addEventListener("resize", function () {
+
+    if (window.innerWidth > 768) {
+        document.querySelector(".nav-links").style.display = "flex";
+    } else {
+        document.querySelector(".nav-links").style.display = "none";
+    }
+
+});
+
+//ao clicar no icone olho muda a senha de invisivel para visivel e vice-versa
 botaoOlho.addEventListener("click", function () {
     if (senha.type === "password") {
         senha.type = "text";
@@ -15,3 +39,4 @@ botaoOlho.addEventListener("click", function () {
     }
 
 });
+
